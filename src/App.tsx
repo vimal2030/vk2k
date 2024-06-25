df21a9b93db261617fcde8954591e59e26c36350
vimal2030 <sash2vs2@gmail.com>
2024-06-25 09:51:28 +0530

    ten

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,32 @@
-import "./App.css";
-// src/index.js or src/App.js
-import "./index.css";
-
-// import { Header } from "./components/Header";
-// import EmblaCarousel from "./components/EmblaCarousel";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Layout from "./Layout/Layout";
 import Home from "./Layout/Routes/Home/Home";
 import About from "./Layout/Routes/About/About";
 import Contact from "./Layout/Routes/Contact/Contact";
+import "./App.css";
+import "./index.css";
 
 function App() {
-  const routes = createBrowserRouter([
-    {
-      path: "/",
-      element: <Layout />,
-      children: [
-        {
-          path: "/",
-          element: <Home />,
-        },
-        {
-          path: "/about",
-          element: <About about={[]} />,
-        },
-        {
-          path: "/contact",
-          element: <Contact />,
-        },
-        // {
-        //   path: "/menu",
-        //   element: <Menu />,
-        //   children: [{ path: "/:id", element: <MenuItem /> }],
-        // },
-      ],
-    },
-  ]);
-
   return (
-    <>
-      <RouterProvider router={routes}></RouterProvider>
-    </>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About about={[]} />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </HashRouter>
   );
-  // return (
-  //   <>
-  //     <Header />
-  //     <EmblaCarousel slides={imagelist} />
-  //   </>
-  // );
 }
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
 export default App;
