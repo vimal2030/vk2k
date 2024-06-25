bf723dddf9e18d14ee848dc7e8f634b0125355ff
vimal2030 <sash2vs2@gmail.com>
2024-06-25 12:32:00 +0530

    bottom

--- src/Layout/Routes/components/BottomBar.tsx
+++ src/Layout/Routes/components/BottomBar.tsx
@@ -1,18 +1,14 @@
 
-import homeIcon from "../../../assets/home_icon.svg";
-import searchIcon from "../../../assets/search_icon.svg";
-import mobileCartIcon from "../../../assets/mobile_cart.svg";
-import accountIcon from "../../../assets/about.jpg";
 
 export const BottomBar = () => {
   const navLinks = [
     {
       name: "Shop",
-      icon: homeIcon,
+      icon: "../../../assets/home_icon.svg",
     },
     {
       name: "Search",
-      icon: searchIcon,
+      icon: "../../../assets/search_icon.svg",
     },
   ];
   return (
@@ -21,33 +17,34 @@ export const BottomBar = () => {
       <div className="z-[3] p-[10px] text-[12px] flex items-center justify-evenly fixed bg-black shadow-shadow bottom-0 md:hidden w-full uppercase">
         {navLinks.map((link, index) => {
           return (
-            <a
-              className="flex flex-col items-center gap-[5px]"
-              href=""
-              key={index}
-            >
-              <img
-                className="h-[25px] w-[25px] bg-contain bg-no-repeat"
-                src={link.icon}
-                alt=""
-              />
-              {link.name}
-            </a>
+            <>
+              <a
+                className="flex flex-col items-center gap-[5px] "
+                href=""
+                key={index}
+              >
+                <img
+                  className="h-[25px] w-[25px] bg-contain bg-no-repeat"
+                  src={link.icon}
+                  alt=""
+                />
+                {link.name}
+              </a>
+            </>
           );
         })}
         <a href="" className="flex flex-col items-center gap-[5px]">
           <img
-            src={mobileCartIcon}
+            src="../src/assets/mobile_cart.svg"
             className="h-[25px] w-[25px] bg-contain bg-no-repeat"
-            alt=""
-          />
+          ></img>
           <span>Cart</span>
         </a>
 
-        <a href="" className="text-brown flex flex-col items-center gap-[3px]">
+        <a href="" className="text-brown  flex flex-col items-center gap-[3px]">
           <span>
             <img
-              src={accountIcon}
+              src="../../../assets/about.jpg"
               className="h-[25px] w-[25px] rounded-[32px]"
               alt=""
             />
