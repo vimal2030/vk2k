c9c630a7a0f04350dc2bbae5441ad8d43e9066c7
vimal2030 <sash2vs2@gmail.com>
2024-06-25 12:58:32 +0530

    index

--- src/Layout/Routes/components/Header.tsx
+++ src/Layout/Routes/components/Header.tsx
@@ -1,6 +1,5 @@
-// import "../../../App.css";
-
 import { useEffect, useState } from "react";
+import cartShoppingIcon from "../../assets/cart_shopping_icon.svg";
 
 export const Header = () => {
   const [isFixed, setIsFixed] = useState(false);
@@ -20,40 +19,37 @@ export const Header = () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+
   const navLinks = ["Home", "Menu", "About", "Contact"];
+
   return (
     <nav
-      className={`w-[100%] h-[40px]  bg-black md:h-[80px] md:bg-transparent flex justify-center border-b-[1px] border-borderBtm z-[2]  px-[2rem] py-[1.5rem] md:py-[0.9rem] md:px-[5rem] 
-        ${isFixed ? "fixed animate-slide-down backdrop-blur" : "absolute"}`}
+      className={`w-full h-40 bg-black md:h-80 md:bg-transparent flex justify-center border-b-1 border-borderBtm z-2 px-2 md:px-5 py-1.5 md:py-0.9 md:px-5 ${
+        isFixed ? "fixed animate-slide-down backdrop-blur" : "absolute"
+      }`}
     >
-      <div className="w-[100dvw] flex items-center justify-between">
-        <div className="flex flex-col items-center font-[700] leading-none">
-          <span className="text-[16px] md:text-[22px]">THOOKU</span>
-          <span className=" text-[14px] md:text-[18px]">BIRYANI</span>
+      <div className="w-full flex items-center justify-between">
+        <div className="flex flex-col items-center font-semibold leading-none">
+          <span className="text-16 md:text-22">THOOKU</span>
+          <span className="text-14 md:text-18">BIRYANI</span>
         </div>
-        <div className="hidden md:flex gap-[60px] items-center w-[600px] uppercase">
-          {navLinks.map((link, index) => {
-            return (
-              <a
-                className="no-underline text-[13px] hover:text-brown hover:transition-all"
-                href=""
-                key={index}
-              >
-                {link}
-              </a>
-            );
-          })}
-          <div className="flex gap-[60px] items-center">
-            <a href="">
-              <img
-                src="../src/assets/cart_shopping_icon.svg"
-                className=" cursor-pointer"
-              ></img>
-            </a>
-
+        <div className="hidden md:flex gap-60 items-center w-600 uppercase">
+          {navLinks.map((link, index) => (
             <a
-              href=""
-              className="px-4 py-3 border border-brown bg-brown text-black rounded-[3px] hover:text-brown hover:bg-transparent hover:transition-all hover:border "
+              href="#"
+              key={index}
+              className="no-underline text-13 hover:text-brown hover:transition-all"
+            >
+              {link}
+            </a>
+          ))}
+          <div className="flex gap-60 items-center">
+            <a href="#">
+              <img src={cartShoppingIcon} className="cursor-pointer" alt="Cart Icon" />
+            </a>
+            <a
+              href="#"
+              className="px-4 py-3 border border-brown bg-brown text-black rounded-3 hover:text-brown hover:bg-transparent hover:transition-all hover:border"
             >
               SignUp
             </a>
